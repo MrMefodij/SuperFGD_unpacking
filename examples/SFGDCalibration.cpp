60336197a7bcb71cb7c5985299f723c4201e9b2a
Maria Kolupanova <kolupanova.ma@phystech.edu>
2022-10-16 11:35:19 +0300

    fix

--- examples/SFGDCalibration.cpp
+++ examples/SFGDCalibration.cpp
@@ -22,7 +22,6 @@ using namespace std;
 
 Int_t npeaks = 30;
 
-<<<<<<< HEAD
 Double_t fpeaks(Double_t *x, Double_t *par) {
    Double_t result = par[0] + par[1]*x[0];
    for (Int_t p=0;p<npeaks;p++) {
@@ -36,8 +35,6 @@ Double_t fpeaks(Double_t *x, Double_t *par) {
    }
    return result;
 }
-=======
->>>>>>> newDAQwords
 
 int main(int argc, char **argv){
     // Проверяем, что файл открылся
@@ -62,7 +59,6 @@ int main(int argc, char **argv){
         Int_t FEB = file_reader.Fill_FEB();
         TDirectory *FEBdir = file_reader.Create_directory(wfile);
 
-<<<<<<< HEAD
         Double_t par[3000];
         for (Int_t iCh=0; iCh<channels_num; iCh++) {
             
@@ -96,58 +92,6 @@ int main(int argc, char **argv){
             #endif /* defined(__PEAKS_C_FIT_AREAS__) */
                   npeaks++;
                   }
-=======
-        hFEBCH[iCh] = file_reader.Print_hFEBCH(iCh);
-        TF1 *func = new TF1("fit",fitf,-3,3,3);
-        func->SetParameters(500,hFEBCH[iCh]->GetMean(),hFEBCH[iCh]->GetRMS());
-        func->SetParNames ("Constant","Mean_value","Sigma");
-        hFEBCH[iCh]->Draw();
-        hFEBCH[iCh]->Fit("fit","R");
-        hFEBCH[iCh]->GetYaxis()->SetTitle("Number of events");
-        hFEBCH[iCh]->GetXaxis()->SetTitle("HG ADC channels");
-        hFEBCH[iCh]->Write();
-       
-        TSpectrum *s = new TSpectrum(2*npeaks);
-        Int_t nfound = s->Search(hFEBCH[iCh],2,"",0.01);
-        printf("Found %d candidate peaks to fit\n",nfound);
-
-//             TH1 *hb = s->Background(hFEBCH[iCh],20,"same");
-//             if (hb) c1->Update();
-//             //estimate linear background using a fitting method
-//             TF1 *fline = new TF1("fline","pol1",0,1000);
-//             hFEBCH[iCh]->Fit("fline","qn");
-//             // Loop on all found peaks. Eliminate peaks at the background level
-//             par[0] = fline->GetParameter(0);
-//             par[1] = fline->GetParameter(1);
-//             npeaks = 0;
-//             Double_t *xpeaks;
-//             xpeaks = s->GetPositionX();
-//             for (auto p=0;p<nfound;p++) {
-//                Double_t xp = xpeaks[p];
-//                Int_t bin = hFEBCH[iCh]->GetXaxis()->FindBin(xp);
-//                Double_t yp = hFEBCH[iCh]->GetBinContent(bin);
-//                if (yp-TMath::Sqrt(yp) < fline->Eval(xp)) continue;
-//                par[3*npeaks] = yp; // "height"
-//                par[3*npeaks+1] = xp; // "mean"
-//                par[3*npeaks+2] = 3; // "sigma"
-//          #if defined(__PEAKS_C_FIT_AREAS__)
-//                par[3*npeaks] *= par[3*npeaks+2 * (TMath::Sqrt(TMath::TwoPi())); // "area"
-//          #endif /* defined(__PEAKS_C_FIT_AREAS__) */
-//                npeaks++;
-//             }
-//             printf("Found %d useful peaks to fit\n",npeaks);
-//             printf("Now fitting: Be patient\n");
-//             TF1 *fit = new TF1("fit",fpeaks,0,1000,2+3*npeaks);
-//             // We may have more than the default 25 parameters
-//             TVirtualFitter::Fitter(hFEBCH[iCh],10+3*npeaks);
-//             fit->SetParameters(par);
-//             fit->SetNpx(1000);
-//             hFEBCH[iCh]->Fit("fit");
-                                    
-        c1->Update();
-        delete hFEBCH[iCh];
-    }
->>>>>>> newDAQwords
 
                // We may have more than the default 25 parameters
             TVirtualFitter::Fitter(hFEBCH[iCh],10+3*npeaks);
