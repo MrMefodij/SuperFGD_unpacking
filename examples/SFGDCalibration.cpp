0f6ebb5ad6374530344f989aae10b7d88e0e8d2a
Maria Kolupanova <kolupanova.ma@phystech.edu>
2022-10-19 14:14:38 +0300

    new legend and eliminated "extra peaks"

--- examples/SFGDCalibration.cpp
+++ examples/SFGDCalibration.cpp
@@ -8,6 +8,7 @@
 #include <TF1.h>
 #include <TMath.h>
 #include <TROOT.h>
+#include <TLegend.h>
 
 //#include "Peaks.h"
 #include "Calibration.h"
@@ -24,12 +25,16 @@ int main(int argc, char **argv){
     
     // Create root file
     string sFileName;
-    string rootFileOutput="Channels_signal.root";
-    TFile wfile(rootFileOutput.c_str(), "recreate");
+    getline(fList,sFileName);
+    string rootFileOutput=GetLocation(sFileName.c_str());
+    rootFileOutput+="_channels_signal.root";
+    TFile wfile = TFile(rootFileOutput.c_str(), "RECREATE");
     TCanvas *c1 = new TCanvas("c1","",0,10,700,500);
     int j = 0;
     map<string,vector<Peaks>> data;
+    //TTree *tCalibSFGD = new TTree("calibSFGD","calibSFGD");
     // Going through data file
+    fList.seekg(0);
     while(getline(fList,sFileName)){
         cout <<sFileName<<endl;
         Int_t channels_num = 256;
@@ -48,20 +53,35 @@ int main(int argc, char **argv){
             hFEBCH[iCh]->GetXaxis()->SetTitle("HG ADC channels");
             string channel = "Channel_" +  to_string(iCh) +'_' + to_string(j);
             data[channel] = cl.Calibration_Par();
-            Int_t hg = (int)(cl.Calibration_Gain());
-            if(hg > 0){
-                cout << channel <<" hg= "<<(int)(cl.Calibration_Gain())<<" error: "<<cl.Calibration_Gain_Error()<<endl;
+            hFEBCH[iCh]->SetStats(0);
+            // write legend
+            if(cl.Calibration_Gain_Error() == cl.Calibration_Gain_Error() ){
+                auto* legend = new TLegend(0.5,0.6,0.9,0.9);
+                string header = "peaks found: " + to_string(data[channel].size());
+                legend->SetHeader(header.c_str());
+                legend -> SetFillColor(0);
+                for(auto i = 0; i < data[channel].size();i++){
+                    string name = "mean peak " + to_string(i) + /*" p. e."*/+" = " + 
+                        to_string(data[channel][i].GetPosition()) + " +/- " + 
+                        to_string(data[channel][i].GetPositionError());
+                    legend->AddEntry((TObject*)0, name.c_str(), "");
+                }
+                string gain = "gain = " + to_string(cl.Calibration_Gain())
+                    + " +/- " + to_string(cl.Calibration_Gain_Error());
+                legend->AddEntry((TObject*)0, gain.c_str(), "");
+                legend->Draw();
+            }else {
+                cout <<"Problem in channel: "<<iCh<< endl;
             }
             c1->Update();
-            hFEBCH[iCh]->Write(); 
+            c1->Write(channel.c_str()); 
+            //hFEBCH[iCh]->Write();
             delete hFEBCH[iCh];
         }
         ++j;
-    }
-        fList.close();
+    }   
+    fList.close();
 
 
     return 0;
 }
-
-//fList.seekg(0); // возврат к началу файла
--- src/Calibration.cpp
+++ src/Calibration.cpp
@@ -32,7 +32,7 @@ Double_t fpeaks(Double_t *x, Double_t *par) {
 void Calibration::Gain_calculstion(const vector<Peaks> & peaks){
     Double_t distance = 0;
     Double_t distance_error = 0;
-    for(auto p=0;p<nfound;p++){
+    for(auto p=0;p<peaks.size();p++){
         if(p>0){
             distance+=(peaks[p].GetPosition()-peaks[p-1].GetPosition());
         } 
@@ -46,37 +46,41 @@ TH1F* Calibration::SFGD_Calibration(TH1F * &h){
     Int_t npeaks = 30;
     TH1F* hFEBCH = h;
     TSpectrum *s = new TSpectrum(2*npeaks);
-    nfound = s->Search(hFEBCH,2,"",0.002);
+    nfound = s->Search(hFEBCH,2,"",0.001);
     Double_t par[3*nfound+4];
-    TF1 *fline = new TF1("fline","pol1",0,1000);
-    hFEBCH->Fit("fline","qn");
-    par[0] = fline->GetParameter(0);
-    par[1] = fline->GetParameter(1);
-    Double_t *xpeaks = s->GetPositionX();
-    for (auto p=0;p<nfound;p++) {
-        Double_t xp = xpeaks[p];
-        Int_t bin = hFEBCH->GetXaxis()->FindBin(xp);
-        Double_t yp = hFEBCH->GetBinContent(bin);
-        if (yp-TMath::Sqrt(yp) < fline->Eval(xp)) continue;
-            par[3*p+2] = yp; // "height"
-            par[3*p+3] = xp; // "mean"
-            par[3*p+4] = 3; // "sigma"
-        #if defined(__PEAKS_C_FIT_AREAS__)
-            par[3*p+2] *= par[3*p+4] * (TMath::Sqrt(TMath::TwoPi())); // "area"
-        #endif /* defined(__PEAKS_C_FIT_AREAS__) */
-    }
+    if(nfound > 0){
+        TF1 *fline = new TF1("fline","pol1",0,1000);
+        hFEBCH->Fit("fline","qn");
+        par[0] = fline->GetParameter(0);
+        par[1] = fline->GetParameter(1);
+        Double_t *xpeaks = s->GetPositionX();
+        for (auto p=0;p<nfound;p++) {
+            Double_t xp = xpeaks[p];
+            Int_t bin = hFEBCH->GetXaxis()->FindBin(xp);
+            Double_t yp = hFEBCH->GetBinContent(bin);
+            if (yp-TMath::Sqrt(yp) < fline->Eval(xp)) continue;
+                par[3*p+2] = yp; // "height"
+                par[3*p+3] = xp; // "mean"
+                par[3*p+4] = 3; // "sigma"
+            #if defined(__PEAKS_C_FIT_AREAS__)
+               par[3*p+2] *= par[3*p+4] * (TMath::Sqrt(TMath::TwoPi())); // "area"
+            #endif /* defined(__PEAKS_C_FIT_AREAS__) */
+        }
 
-    // We may have more parameters
-    TVirtualFitter::Fitter(hFEBCH,10+3*nfound);
-    for (auto p=0;p<nfound;p++) {
-        //the fitting takes place at the interval of 3 sigma
-        TF1 * fit_1 = new TF1("fit_1",fpeaks,par[3*p+3]-3*par[3*p+4],par[3*p+3]+3*par[3*p+4],2+3*nfound);
-        Double_t error = hFEBCH->GetBinError(3*p+3);
-        fit_1->SetParameters(par);
-        fit_1->SetNpx(1000);
-        hFEBCH->Fit("fit_1","qr+");
-        Peaks peak = {par[3*p+3],error,(int)(par[3*p+2]),par[3*p+4]};
-        peaks.push_back(peak);
+        // We may have more parameters
+        TVirtualFitter::Fitter(hFEBCH,10+3*nfound);
+        for (auto p=0;p<nfound;p++) {
+            //the fitting takes place at the interval of 3 sigma
+            if(p==0 || (p > 0 && (par[3*p+3] - par[3*(p-1)+3]) > 0)){
+                TF1 * fit_1 = new TF1("fit_1",fpeaks,par[3*p+3]-3*par[3*p+4],par[3*p+3]+3*par[3*p+4],2+3*nfound);
+                fit_1->SetParameters(par);
+                fit_1->SetNpx(1000);
+                hFEBCH->Fit("fit_1","qr+");
+                Double_t error = fit_1->GetParError(3*p+3);
+                Peaks peak = {par[3*p+3],error,(int)(par[3*p+2]),par[3*p+4]};
+                peaks.push_back(peak);
+            }
+        }
     }
     Gain_calculstion(peaks);
     return hFEBCH;
